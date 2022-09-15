a8d62cb4ee4450cd11a6e32288f0ccd81bdf88aa
Leonardo C <leonardocoel98@gmail.com>
2022-09-15 16:46:30 -0300

    refactor:implement zod schema in IVehicle

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,14 @@
-export interface IVehicle {
-  model: string;
-  year: number;
-  color: string;
-  status?: boolean;
-  buyValue: number;
-}
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+export default vehicleZodSchema;
+export { IVehicle };
