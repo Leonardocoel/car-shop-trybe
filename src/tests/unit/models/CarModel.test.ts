84ccb103f7a49faf63ecd5188c0cff7ee27c1616
Leonardo C <leonardocoel98@gmail.com>
2022-09-17 16:06:28 -0300

    test: add car model unit test and implement create test

---
+++ src/tests/unit/models/CarModel.test.ts
@@ -0,0 +1,27 @@
+import * as sinon from "sinon";
+import chai from "chai";
+const { expect } = chai;
+
+import CarModel from "../../../models/Car";
+import { Model } from "mongoose";
+import { carMock, carMockWithId } from "../../mocks/carMock";
+
+describe("Car Model", () => {
+  const carModel = new CarModel();
+
+  before(() => {
+    sinon.stub(Model, "create").resolves(carMockWithId);
+  });
+
+  describe("add new car", () => {
+    it("when the attributes are correct a new car is returned", async () => {
+      const newCar = await carModel.create(carMock);
+      expect(newCar).to.be.eql(carMockWithId);
+    });
+  });
+
+  // describe('', () => {
+
+  //   it('', async () => {});
+  // })
+});
