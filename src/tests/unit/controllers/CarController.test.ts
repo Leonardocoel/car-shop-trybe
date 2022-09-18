19d5f18cb5243321ef23f6e0244ff233aff06646
Leonardo C <leonardocoel98@gmail.com>
2022-09-17 23:18:10 -0300

    test: implement find all cars test in CarController

--- src/tests/unit/controllers/CarController.test.ts
+++ src/tests/unit/controllers/CarController.test.ts
@@ -6,7 +6,7 @@ import CarModel from "../../../models/Car";
 import CarsService from "../../../services/Car.service";
 import CarController from "../../../controllers/Car.controller";
 import { IRequest, IResponse } from "../../../interfaces/IController";
-import { carMock, carMockWithId } from "../../mocks/carMock";
+import { allCarsMockWithId, carMock, carMockWithId } from "../../mocks/carMock";
 
 describe("Car Controller", () => {
   const carModel = new CarModel();
@@ -14,12 +14,10 @@ describe("Car Controller", () => {
   const carController = new CarController(carService);
 
   const req = {} as IRequest;
-  const res = {} as IResponse;
 
   before(async () => {
-    sinon.stub(carService, 'create').resolves(carMockWithId);
-
-    
+    sinon.stub(carService, "create").resolves(carMockWithId);
+    sinon.stub(carService, "findAll").resolves(allCarsMockWithId);
   });
 
   after(() => {
@@ -27,13 +25,20 @@ describe("Car Controller", () => {
   });
 
   describe("add new car", () => {
-    
-
     it("when the attributes are correct a new car is returned with status 201", async () => {
       req.body = carMock;
       const newCar = await carController.create(req);
 
-      expect(newCar).to.be.eql({status: 201, body: carMockWithId})
+      expect(newCar).to.be.eql({ status: 201, body: carMockWithId });
     });
+  });
+
+  describe("find all cars", () => {
+    it("when there are cars, return full list",async () => {
+      
+      const allCars = await carController.findAll();
+      
+      expect(allCars).to.be.eql({status: 200, body: allCarsMockWithId});
+    })
   });
 });
